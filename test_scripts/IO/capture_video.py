5b459576f2b310bc0fe6f3c9646d68e54e158413
nanoTech Corporation <thiagofigcosta@hotmail.com>
2018-06-21 09:51:43 -0300

    capture video script

--- test_scripts/IO/capture_video.py
+++ test_scripts/IO/capture_video.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import platform
+
+if platform.system()=='Linux':
+	import pyscreenshot as ImageGrab
+else:
+	from PIL import ImageGrab
+	
+import numpy as np
+import cv2
+
+img = ImageGrab.grab(bbox=(100,10,400,780)) #bbox specifies specific region (bbox= x,y,width,height *starts top-left)
+img_np = np.array(img) #this is the array obtained from conversion
+frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+cv2.imshow("test", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
